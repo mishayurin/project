3fbd76748cb1c93ac6ff09893b330ede567d0157
ivan <Mr.Chudvan@yandex.ru>
2022-05-25 11:06:47 +0300

    create relations between models

--- budgetmanagement/backend/models.py
+++ budgetmanagement/backend/models.py
@@ -4,8 +4,7 @@ from django.db import models
 # In this model file to communicate with the database
 
 class Client(models.Model):
-    # id = models.AutoField()
-    Client_ID = models.AutoField()
+    Client_ID = models.AutoField(primary_key=True)
     Phone = models.SlugField(max_length=25)
     Email = models.EmailField(max_length=254)
     Date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
@@ -13,22 +12,22 @@ class Client(models.Model):
     Wallet_balance = models.BigIntegerField()
 
 
-class Expenses(models.Model):
-    Expenses_ID = models.AutoField()
-    Amount = models.IntegerField()  # models.PositiveIntegerField()
-    Expense_Date = models.DateTimeField(auto_now_add=True)
-    Client_ID = models.AutoField()
-    Limits_ID = models.AutoField()
-    Expenses_types_ID = models.models.AutoField()
-
-
 class Expenses_Types(models.Model):
-    Expenses_types_ID = models.AutoField()
+    Expenses_types_ID = models.AutoField(primary_key=True)
     Category = models.CharField(max_length=255)
     Name_of_expenses_types = models.CharField(max_length=255)
 
 
 class Limits(models.Model):
-    Limits_ID = models.AutoField()
+    Limits_ID = models.AutoField(primary_key=True)
     Amount_of_limit = models.IntegerField()
     Date_time_gap = models.DateTimeField()
+
+
+class Expenses(models.Model):
+    Expenses_ID = models.AutoField(primary_key=True)
+    Amount = models.IntegerField()  # models.PositiveIntegerField()
+    Expense_Date = models.DateTimeField(auto_now_add=True)
+    Client_ID = models.ForeignKey(Client, on_delete = models.CASCADE)
+    Limits_ID = models.ForeignKey(Limits, on_delete = models.CASCADE)
+    Expenses_types_ID = models.ForeignKey(Expenses_Types, on_delete = models.CASCADE)
